08e236a7d1dc943f78447da61c0b4813b41e9b2d
MD ABIR HASAN MUNNA <mtmunna82@gmail.com>
2022-10-23 18:46:18 +0600

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ import controller as cnt
 
 time.sleep(2.0)
 
-mp_draw=mp.solutions.drawing_utils
-mp_hand=mp.solutions.hands
+mp_hands=mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+
 
 
 tipIds=[4,8,12,16,20]
 
 video=cv2.VideoCapture(0)
 
-with mp_hand.Hands(min_detection_confidence=0.5,
-               min_tracking_confidence=0.5) as hands:
+with mp_hands.Hands(min_detection_confidence=0.8,
+               min_tracking_confidence=0.5, max_num_hands=1) as hands:
     while True:
         ret,image=video.read()
         image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -23,28 +24,50 @@ with mp_hand.Hands(min_detection_confidence=0.5,
         results=hands.process(image)
         image.flags.writeable=True
         image=cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        lmList=[]
+       
+        fingerCount = 0
+
         if results.multi_hand_landmarks:
-            for hand_landmark in results.multi_hand_landmarks:
-                myHands=results.multi_hand_landmarks[0]
-                for id, lm in enumerate(myHands.landmark):
-                    h,w,c=image.shape
-                    cx,cy= int(lm.x*w), int(lm.y*h)
-                    lmList.append([id,cx,cy])
-                mp_draw.draw_landmarks(image, hand_landmark, mp_hand.HAND_CONNECTIONS)
-        fingers=[]
-        if len(lmList)!=0:
-            if lmList[tipIds[0]][1] > lmList[tipIds[0]-1][1]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
-            for id in range(1,5):
-                if lmList[tipIds[id]][2] < lmList[tipIds[id]-2][2]:
-                    fingers.append(1)
-                else:
-                    fingers.append(0)
-            total=fingers.count(1)
+
+            for hand_landmarks in results.multi_hand_landmarks:
+                # Get hand index to check label (left or right)
+                handIndex = results.multi_hand_landmarks.index(hand_landmarks)
+                handLabel = results.multi_handedness[handIndex].classification[0].label
+
+                # Set variable to keep landmarks positions (x and y)
+                handLandmarks = []
+
+                # Fill list with x and y positions of each landmark
+                for landmarks in hand_landmarks.landmark:
+                    handLandmarks.append([landmarks.x, landmarks.y])
+
+                if handLabel == "Left" and handLandmarks[4][0] > handLandmarks[3][0]:
+                    fingerCount = fingerCount + 1
+                elif handLabel == "Right" and handLandmarks[4][0] < handLandmarks[3][0]:
+                    fingerCount = fingerCount + 1
+
+                # Other fingers: TIP y position must be lower than PIP y position,
+                #   as image origin is in the upper left corner.
+                if handLandmarks[8][1] < handLandmarks[6][1]:  # Index finger
+                    fingerCount = fingerCount + 1
+                if handLandmarks[12][1] < handLandmarks[10][1]:  # Middle finger
+                    fingerCount = fingerCount + 1
+                if handLandmarks[16][1] < handLandmarks[14][1]:  # Ring finger
+                    fingerCount = fingerCount + 1
+                if handLandmarks[20][1] < handLandmarks[18][1]:  # Pinky
+                    fingerCount = fingerCount + 1
+
+                # Draw hand landmarks
+                mp_drawing.draw_landmarks(
+                    image,
+                    hand_landmarks,
+                    mp_hands.HAND_CONNECTIONS,
+                    mp_drawing.DrawingSpec(color=(80, 15, 245), thickness=2, circle_radius=4),
+                    mp_drawing.DrawingSpec(color=(227, 72, 16), thickness=3, circle_radius=2),)
+
+            total = fingerCount
             cnt.led(total)
+
             if total==0:
                 cv2.rectangle(image, (20, 300), (270, 425), (0, 255, 0), cv2.FILLED)
                 cv2.putText(image, "0", (45, 375), cv2.FONT_HERSHEY_SIMPLEX,
